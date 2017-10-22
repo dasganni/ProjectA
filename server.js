dd98dc2d571546a033c23b91fb2cc379da1ba0ea
dasganni <20495026+dasganni@users.noreply.github.com>
2017-10-22 13:18:44 +0200

    socket io disconnect and username forwarding (last not working)

--- server.js
+++ server.js
@@ -24,8 +24,12 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-});
+    socket.on('username', function(username));
+    console.log(username + 'connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+  });
 
 // Password Hash
 const passwordHash = require('password-hash');
